624593a01a26d79609a9f014f029cfe33cd8813b
hoholabs <johnpaulhoran@gmail.com>
2022-01-19 21:11:49 -0500

    inital files set up with webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',  
+  
+  entry: './src/index.js',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+     title: 'Development',
+    }),
+  ],
+
+  devServer: {
+    static: './dist',
+  },
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
