ca06c9905e6ab6bb7ae1346570c47c2fcca0c8cf
hoholabs <johnpaulhoran@gmail.com>
2022-01-22 10:15:07 -0500

    added new tab function, it works

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,44 @@
+//Get main container from page
 let mainContainer = document.getElementById('main-container');
 
+//Create navigation bar
 let navBar = document.createElement("nav");
 navBar.id = "nav"
+navBar.style.display = "grid";
+navBar.textContent = "NAV BAR";
 
+//ceate tab bar
+let tabBar = document.createElement("div");
+tabBar.id = "tab-bar";
+tabBar.textContent = "TAB BAR";
+    //create new tab button
+    let newTabBtn = document.createElement('button');
+    newTabBtn.id = "new-tab-button";
+    newTabBtn.textContent = "+";
+    tabBar.append(newTabBtn);
+    //give new tab busson functionalty
+    newTabBtn.addEventListener('click',newTabPrompt);
+    function newTabPrompt(){
+        let newTab = prompt("New tab name:");
+        addTab(newTab);
+    }
+
+
+navBar.append(tabBar);
+
+//add tab to Navbar
+
+function addTab(name){
+    let newTab = document.createElement("div");
+    newTab.className = "tab";
+    tabBar.append(newTab);
+};
+
+// addTab(default);
 
 //Add Nav to top of page
-function addNav(){
-    console.log('addnav')
+export default function addNav(){
     mainContainer.append(navBar);
 }
 
-export default addNav;
+//export default addNav;
